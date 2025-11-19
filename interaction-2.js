6b39aa34ddd408e6ccd52bd2475e0d4583f04faa
Bhavya Sehgal <bsehgal@kth.se>
2025-11-19 14:58:09 +0100

    g

--- interaction-2.js
+++ interaction-2.js
@@ -90,7 +90,7 @@ function deviceTurned() {
 function deviceShaken() {
   shaketimer = millis();
   statusLabels[0].style("color", "pink");
-  playAudio();
+  // playAudio();
 }
 
 function getMinMaxParam(address) {
